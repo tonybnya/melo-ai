05afcd5c7181728176ba2d6ceb95e53946e1f36d
tonybnya <tonybnya@gmail.com>
2026-05-11 12:16:31 +0100

    add an utilities file for prompt and helper functions for questions parsing

---
+++ utils.py
@@ -0,0 +1,13 @@
+"""
+Script Name : utils.py
+Description : Utilities for prompt and helper functions for questions parsing
+Author      : @tonybnya
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
