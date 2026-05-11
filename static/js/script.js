fe6e95f3d0f38fce4d40b2d05a4f6e15797aa52b
tonybnya <tonybnya@gmail.com>
2026-05-11 13:06:23 +0100

    feat: add a dynamic year on footer component

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 // current date in header
 (function () {
   const d = new Date();
+  document.getElementById('footer-year').textContent = d.getFullYear();
   document.getElementById('current-date').textContent = d.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 })();
