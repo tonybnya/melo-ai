4e4604d028d15fb9885110b30e830c721b36b37d
tonybnya <tonybnya@gmail.com>
2026-05-11 12:50:05 +0100

    style: add the header component

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,5 @@
-console.log('Melo AI');
+// current date in header
+(function () {
+  const d = new Date();
+  document.getElementById('current-date').textContent = d.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+})();
